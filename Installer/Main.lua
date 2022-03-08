fab468d907d74a9e571a6e835c522522f8b758d7
Yaroslav-Nesst <59181976+Yaroslav-Nesst@users.noreply.github.com>
2022-03-08 23:07:32 +0200

    Update Main.lua

--- Installer/Main.lua
+++ Installer/Main.lua
@@ -17,7 +17,7 @@ local EEPROMProxy, internetProxy, GPUProxy =
 GPUProxy.bind(getComponentAddress("screen"))
 local screenWidth, screenHeight = GPUProxy.getResolution()
 
-local repositoryURL = "https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/"
+local repositoryURL = "https://raw.githubusercontent.com/Yaroslav-Nesst/MineOsForked/master/"
 local installerURL = "Installer/"
 local EFIURL = "EFI/Minified.lua"
 
@@ -42,13 +42,13 @@ end
 
 local function title()
 	local y = math.floor(screenHeight / 2 - 1)
-	centrizedText(y, 0x2D2D2D, "MineOS")
+	centrizedText(y, 0xFF6347, "MineOsForked")
 
 	return y + 2
 end
 
 local function status(text, needWait)
-	centrizedText(title(), 0x878787, text)
+	centrizedText(title(), 0xFF6347, text)
 
 	if needWait then
 		repeat
@@ -155,7 +155,7 @@ end
 
 -- If there's no suitable HDDs found - then meow
 if not temporaryFilesystemProxy then
-	status("No appropriate filesystem found", true)
+	status("No drives found!", true)
 	return
 end
 
@@ -235,7 +235,7 @@ window:addChild(GUI.panel(1, 1, window.width, window.height, 0xE1E1E1))
 
 -- Top menu
 local menu = workspace:addChild(GUI.menu(1, 1, workspace.width, 0xF0F0F0, 0x787878, 0x3366CC, 0xE1E1E1))
-local installerMenu = menu:addContextMenuItem("MineOS", 0x2D2D2D)
+local installerMenu = menu:addContextMenuItem("MineOsForked", 0x2D2D2D)
 installerMenu:addItem("Shutdown").onTouch = function()
 	computer.shutdown()
 end
