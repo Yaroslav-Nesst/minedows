a9f8e87350853c086f34a4256c9114237c55f3ad
Yaroslav-Nesst <yariknesterenko9@gmail.com>
2022-11-13 18:52:31 +0300

    Update Main.lua

--- Installer/Main.lua
+++ Installer/Main.lua
@@ -234,7 +234,7 @@ window.localX, window.localY = math.ceil(workspace.width / 2 - window.width / 2)
 window:addChild(GUI.panel(1, 1, window.width, window.height, 0xE1E1E1))
 
 -- Top menu
-local menu = workspace:addChild(GUI.menu(1, 1, workspace.width, 0x38761d, 0x38761d, 0x3366CC, 0xE1E1E1))
+local menu = workspace:addChild(GUI.menu(1, 1, workspace.width, 0x38761d, 0x38761d, 0x3366CC, 0x38761d))
 local installerMenu = menu:addContextMenuItem("Minedows", 0x2D2D2D)
 installerMenu:addItem("Shutdown").onTouch = function()
 	computer.shutdown()
