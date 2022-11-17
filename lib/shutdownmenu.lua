c2a189e05b0ab0c2de9097292df5436db65622d3
Yaroslav-Nesst <yariknesterenko9@gmail.com>
2022-11-17 22:40:39 +0300

    fgdgfg

--- lib/shutdownmenu.lua
+++ lib/shutdownmenu.lua
@@ -7,8 +7,13 @@ local window3 = workspace:addChild(GUI.titledWindow(50, 22, 60, 20, "Titled wind
 -- Attach an single cell layout to it
 local layout = window3:addChild(GUI.layout(1, 2, window3.width, window3.height - 1, 1, 1))
 -- Add some stuff to layout
-layout:addChild(GUI.button(1, 1, 36, 3, 0xB4B4B4, 0xFFFFFF, 0x969696, 0xB4B4B4, "Press me"))
-layout:addChild(GUI.button(1, 1, 36, 3, 0xB4B4B4, 0xFFFFFF, 0x969696, 0xB4B4B4, "And me"))
+layout:addChild(GUI.text(15, 5, 0x000000, "Are you sure you want to:"))
+layout:addChild(GUI.text(18, 7, 0x000000, "Shut down the computer?"))
+layout:addChild(GUI.text(18, 9, 0x000000, "Restart the computer?"))
+layout:addChild(GUI.text(18, 11, 0x000000, "Close all programs and log out from user?"))
+layout:addChild(GUI.text(15, 13, 0x000000, "Current mode set:"))
+layout:addChild(GUI.text(33, 13, 0x000000, "None"))
+layout:addChild(GUI.image(2, 5, image.load("/Temp/Installer/Pictures/Pc.pic")))
 --------------------------------------------------------------------------------
 
 workspace:draw()
