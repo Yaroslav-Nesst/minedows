dda347cebc16568c0c350ece788a8a0dec688a40
Yaroslav-Nesst <yariknesterenko9@gmail.com>
2022-11-17 22:15:41 +0300

    ,ol

--- lib/shutdownmenu.lua
+++ lib/shutdownmenu.lua
@@ -15,46 +15,48 @@ modeset = "None"
 
 blanka = ("○")
 
-local workspace, shutdown, menu = system.addWindow(GUI.titledWindow(50, 22, 60, 15, "Shut Down Minedows", true))
+local workspace = GUI.workspace()
 
-local layout = workspace:addChild(GUI.layout(1, 2, shutdown.width, shutdown.height - 1, 1, 1))
-workspace:addChild(GUI.text(15, 5, 0x000000, "Are you sure you want to:"))
-workspace:addChild(GUI.text(18, 7, 0x000000, "Shut down the computer?"))
-workspace:addChild(GUI.text(18, 9, 0x000000, "Restart the computer?"))
-workspace:addChild(GUI.text(18, 11, 0x000000, "Close all programs and log out from user?"))
-workspace:addChild(GUI.text(15, 13, 0x000000, "Current mode set:"))
-workspace:addChild(GUI.text(33, 13, 0x000000, "None"))
-workspace:addChild(GUI.image(2, 5, image.load("Temp/Installer/Pictures/Pc.pic")))
+local window, shutdown = workspace:addChild(GUI.titledWindow(50, 22, 60, 15, "Shut Down Minedows", true))
+
+local layout = window:addChild(GUI.layout(1, 2, shutdown.width, shutdown.height - 1, 1, 1))
+window:addChild(GUI.text(15, 5, 0x000000, "Are you sure you want to:"))
+window:addChild(GUI.text(18, 7, 0x000000, "Shut down the computer?"))
+window:addChild(GUI.text(18, 9, 0x000000, "Restart the computer?"))
+window:addChild(GUI.text(18, 11, 0x000000, "Close all programs and log out from user?"))
+window:addChild(GUI.text(15, 13, 0x000000, "Current mode set:"))
+window:addChild(GUI.text(33, 13, 0x000000, "None"))
+window:addChild(GUI.image(2, 5, image.load("Temp/Installer/Pictures/Pc.pic")))
 
 
-local shutdownb = workspace:addChild(GUI.button(15, 7, 1, 1, 0xFFFFFF, 0x555555, 0xC4C4C4, 0xFFFFFF, blanka))
+local shutdownb = window:addChild(GUI.button(15, 7, 1, 1, 0xFFFFFF, 0x555555, 0xC4C4C4, 0xFFFFFF, blanka))
 shutdownb.animated = false
 shutdownb.onTouch = function()
-    workspace:addChild(GUI.text(33, 13, 0x000000, "        "))
-    workspace:addChild(GUI.text(33, 13, 0x000000, "Shutdown"))
+    window:addChild(GUI.text(33, 13, 0x000000, "        "))
+    window:addChild(GUI.text(33, 13, 0x000000, "Shutdown"))
     modeset = "Shutdown"
     workspace:draw()
 end
 
-local rebootb = shutdown:addChild(GUI.button(15, 9, 1, 1, 0xFFFFFF, 0x555555, 0xC4C4C4, 0xFFFFFF, blanka))
+local rebootb = window:addChild(GUI.button(15, 9, 1, 1, 0xFFFFFF, 0x555555, 0xC4C4C4, 0xFFFFFF, blanka))
 rebootb.animated = false
 rebootb.onTouch = function()
-    workspace:addChild(GUI.text(33, 13, 0x000000, "        "))
-    workspace:addChild(GUI.text(33, 13, 0x000000, "Reboot"))
+    window:addChild(GUI.text(33, 13, 0x000000, "        "))
+    window:addChild(GUI.text(33, 13, 0x000000, "Reboot"))
     modeset = "Reboot"
     workspace:draw()
 end
 
-local logoutb = workspace:addChild(GUI.button(15, 11, 1, 1, 0xFFFFFF, 0x555555, 0xC4C4C4, 0xFFFFFF, blanka))
+local logoutb = window:addChild(GUI.button(15, 11, 1, 1, 0xFFFFFF, 0x555555, 0xC4C4C4, 0xFFFFFF, blanka))
 logoutb.animated = false
 logoutb.onTouch = function()
-    workspace:addChild(GUI.text(33, 13, 0x000000, "        "))
-    workspace:addChild(GUI.text(33, 13, 0x000000, "Logout"))
+    window:addChild(GUI.text(33, 13, 0x000000, "        "))
+    window:addChild(GUI.text(33, 13, 0x000000, "Logout"))
     workspace:draw()
     modeset = "Logout"
 end
 
-local okay = workspace:addChild(GUI.button(44, 13, 5, 1, 0xFFFFFF, 0x555555, 0xC4C4C4, 0xFFFFFF, "OK"))
+local okay = window:addChild(GUI.button(44, 13, 5, 1, 0xFFFFFF, 0x555555, 0xC4C4C4, 0xFFFFFF, "OK"))
 okay.animated = false
 okay.onTouch = function()
   if modeset == "Shutdown" then
