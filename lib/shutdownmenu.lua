3300d730838b9efc0b8d42e957d451776da6ea7a
Yaroslav-Nesst <yariknesterenko9@gmail.com>
2022-11-19 18:02:45 +0300

    Update shutdownmenu.lua

--- lib/shutdownmenu.lua
+++ lib/shutdownmenu.lua
@@ -17,13 +17,13 @@ blanka = ("○")
 
 local window3 = workspace:addChild(GUI.titledWindow(50, 22, 60, 20, "Titled window example", true))
 -- Attach an single cell layout to it
-local layout = window3:addChild(GUI.layout(1, 2, window3.width, window3.height - 1, 1, 1))
-layout:addChild(GUI.text(15, 5, 0x000000, "Are you sure you want to:"))
-layout:addChild(GUI.text(18, 7, 0x000000, "Shut down the computer?"))
-layout:addChild(GUI.text(18, 9, 0x000000, "Restart the computer?"))
-layout:addChild(GUI.text(18, 11, 0x000000, "Close all programs and log out from user?"))
-layout:addChild(GUI.text(15, 13, 0x000000, "Current mode set:"))
-layout:addChild(GUI.text(33, 13, 0x000000, "None"))
+--local layout = window3:addChild(GUI.layout(1, 2, window3.width, window3.height - 1, 1, 1))
+window3:addChild(GUI.text(15, 5, 0x000000, "Are you sure you want to:"))
+window3:addChild(GUI.text(18, 7, 0x000000, "Shut down the computer?"))
+window3:addChild(GUI.text(18, 9, 0x000000, "Restart the computer?"))
+window3:addChild(GUI.text(18, 11, 0x000000, "Close all programs and log out from user?"))
+window3:addChild(GUI.text(15, 13, 0x000000, "Current mode set:"))
+window3:addChild(GUI.text(33, 13, 0x000000, "None"))
 
 
 --local shutdownb = shutdown:addChild(GUI.button(15, 7, 1, 1, 0xFFFFFF, 0x555555, 0xC4C4C4, 0xFFFFFF, blanka))
